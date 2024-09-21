0fb8a0cf9ca2951085aac275aeba89848f677545
Felipe Richter <frichterc@gmail.com>
2024-09-21 19:57:28 -0300

    feat: add modules, controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { CommunitiesModule } from './communities/communities.module';
+import { MarketsModule } from './markets/markets.module';
+import { ProductsModule } from './products/products.module';
+import { CommunitesService } from './communites/communites.service';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, CommunitiesModule, MarketsModule, ProductsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CommunitesService],
 })
 export class AppModule {}
