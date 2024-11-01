a4dec644e7b93cd76d14c8827c9cb79381b81763
Felipe Richter <frichterc@gmail.com>
2024-11-01 03:10:06 -0300

    feat: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
 import { CommunitiesModule } from './communities/communities.module';
 import { MarketsModule } from './markets/markets.module';
 import { ProductsModule } from './products/products.module';
-import { CommunitesService } from './communites/communites.service';
+import { UserCommunityController } from './user-community/user-community.controller';
+import { UserCommunityService } from './user-community/user-community.service';
+import { UsersModule } from './users/users.module';
+import { DatabaseTestService } from './database/database-test.service';
 
 @Module({
   imports: [
@@ -15,8 +18,9 @@ import { CommunitesService } from './communites/communites.service';
     CommunitiesModule,
     MarketsModule,
     ProductsModule,
+    DatabaseModule,
   ],
-  controllers: [AppController],
-  providers: [AppService, CommunitesService],
+  controllers: [AppController, UserCommunityController],
+  providers: [AppService, UserCommunityService, DatabaseTestService],
 })
 export class AppModule {}
