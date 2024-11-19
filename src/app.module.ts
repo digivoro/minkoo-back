15128dd441d6dfef650e009d4b303f77443376d0
Felipe Richter <frichterc@gmail.com>
2024-11-19 17:24:02 -0300

    chore: remove unused modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,21 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { CommunitiesModule } from './communities/communities.module';
-import { MarketsModule } from './markets/markets.module';
-import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { DatabaseTestService } from './database/database-test.service';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [
-    UsersModule,
-    CommunitiesModule,
-    MarketsModule,
-    ProductsModule,
-    DatabaseModule,
-    AuthModule,
-  ],
+  imports: [UsersModule, CommunitiesModule, DatabaseModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, DatabaseTestService],
 })
--- src/markets/markets.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('markets')
-export class MarketsController {}
